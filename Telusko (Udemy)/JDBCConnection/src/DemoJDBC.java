0b9846627ff92439a96403cedd3657e878d9b530
Mayank <mayankanand2701@gmail.com>
2025-02-19 22:56:05 +0530

    Adding complete JDBC Code

--- Telusko (Udemy)/JDBCConnection/src/DemoJDBC.java
+++ Telusko (Udemy)/JDBCConnection/src/DemoJDBC.java
@@ -18,8 +18,6 @@ public class DemoJDBC
 		String url="jdbc:postgresql://localhost:5432/demo";
 		String userName="postgres";
 		String password="root";
-		String query="select sname from student where sid=1";
-		String query1="select * from student";
 		
 		// Step 2 : Load and Register (Optional Step)
 		Class.forName("org.postgresql.Driver");
@@ -35,6 +33,9 @@ public class DemoJDBC
 		
 		// -------------------------------------------------------------------------
 		// READ
+		
+		// String query="select sname from student where sid=1";
+		// String query1="select * from student";
 		
 		// ResultSet rs=st.executeQuery(query1);
 		
@@ -54,11 +55,38 @@ public class DemoJDBC
 		// --------------------------------------------------------------------------
 		// CREATE (Insert)
 		
+		// String query="insert into student values (5,'Risabh',100)";
+		// boolean status=st.execute(query); // will return false but in the db data will be reflected
+		// System.out.println("Data got inserted Successfully ? "+status);
 		
+		// --------------------------------------------------------------------------
+		// UPDATE 
+		// String query="update student set sname='Risav' where sid=5";
+		// st.execute(query); // updated through this
 		
+		// ---------------------------------------------------------------------------
+		// DELETE
+		// String query="delete from student where sid=2";
+		// st.execute(query); // deleted through this
 		
-		
-		
+		// PREDPARED STATEMENTS 
+		int sid=101;
+		String name="Max";
+		int marks=40;
+		// Using this statement we can insert the day but it looks tedious task and
+		// it can lead to sql injection problem so prepared statements solve this problem
+		// values are coming from input from users
+		// String query="insert into student values("+sid+",'"+name+"',"+marks+")";
+		String query= "insert into student values (?,?,?)";
+		PreparedStatement st1=con.prepareStatement(query);
+		int sid1=102;
+		String name1="Raghanv";
+		int marks1=90;
+		st1.setInt(1, sid1);
+		st1.setString(2, name1);
+		st1.setInt(3, marks1);
+		st1.execute(); 
+		 
 		// Step 6 : Close
 		con.close();
 		System.out.println("Connection Closed with Database!");
